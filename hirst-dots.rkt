6d53c540f531c4038d2073d9d06c8faee303af5c
Dustin McMahon <85696413+dustinmcmahon@users.noreply.github.com>
2022-09-21 12:09:05 -0400

    Project Complete
    - finished csc344 project

---
+++ hirst-dots.rkt
@@ -0,0 +1,39 @@
+#lang racket
+
+( require 2htdp/image )
+
+;create a random color 'object'
+( define ( random-color ) ( color ( rgb-value ) ( rgb-value ) ( rgb-value ) ) )
+
+;create a random number in the rgb value range
+( define ( rgb-value ) ( random 256 ) )
+
+;single hirst dot, of random color
+( define ( hirst-dot )
+   ( beside ( circle 15 'solid ( random-color ) ) ( square 20 'solid 'white ) )
+)
+
+;row of dots
+( define ( hirst-row n )
+   ( cond
+      ( ( = n 1 ) ( hirst-dot ) )
+      ( else
+        ( beside ( hirst-dot ) ( hirst-row ( - n 1 ) ) )
+      )
+   )
+)
+
+;rectangle of hirst dots
+( define ( hirst-rectangle row column )
+   ( cond
+      ( ( = row 1 ) ( hirst-row column ) )
+      ( else ( above ( hirst-row column ) ( square 20 'solid 'white ) ( hirst-rectangle ( - row 1 ) column ) ) )
+   )
+)
+
+;box of dots
+( define ( hirst-dots n )
+   ( cond
+      ( ( > n 0 ) ( hirst-rectangle n n ) )
+   )
+)
